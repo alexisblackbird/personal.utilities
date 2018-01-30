9610927f263cb2d53a6e24ce128423474031931c
alexisblackbird <alexis.strazds@gmail.com>
2018-01-30 10:49:15 -0500

    fixed varname_label() not exporting/documentation

--- R/varname_label.R
+++ R/varname_label.R
@@ -1,16 +1,15 @@
 #' varname_label
-#'  
-#'  Renames variables based on text parsed from a data key.
-#'  
-#'  Renames variables by parsing text from descriptions of the variable. 
-#'  Must be in the format of a data frame (named datakey) with variables matching the data set (named data), and containing only a single string.
-#'  Intended to work with Qualtrics' output of question text. 
-#'  
-#'  @param vargrp String pattern to rename, renames all variables with this pattern in them.
-#'  @param label A string common label to preface the new variable name with. 
-#'  @param pattern A string section to be removed from labels in the data key, such that any remaining text is added to the variable name.
-#'  @param key True returns the renamed data key, false returns the data. Defaults to false.
-#'  @export
+#'
+#' Renames variables based on text parsed from a data key.
+#'
+#' Renames variables by parsing text from descriptions of the variable.
+#' Must be in the format of a data frame (named datakey) with variables matching the data set (named data), and containing only a single string.
+#' Intended to work with Qualtrics' output of question text.
+#' @param vargrp String pattern to rename, renames all variables with this pattern in them.
+#' @param label A string common label to preface the new variable name with.
+#' @param pattern A string section to be removed from labels in the data key, such that any remaining text is added to the variable name.
+#' @param key True returns the renamed data key, false returns the data. Defaults to false.
+#' @export
 
 varname_label <- function(vargrp, label, pattern, key = FALSE){
   for(i in names(data)){
